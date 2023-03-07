13e267e46f10f9c732b4c6b90b0fc32753387515
Roman Yakymiuk <romeronice93@gmail.com>
2023-03-07 14:27:46 +0200

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,108 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Welcome to Your Vue.js App" />
+  <header class="header">
+    <div class="header__top">
+      <div class="header__logo logo">
+        <img src="./assets/logo.png" alt="GMT India Logo" />
+      </div>
+      <button class="header__burger burger" @click="handleMenu"></button>
+      <Nav :burgerVisibility="burgerVisibility" @close="handleMenu" />
+    </div>
+    <h1 class="header__title title">
+      Turkey dolor sit amet, consectetur adipiscing elit
+    </h1>
+  </header>
+  <section class="section">
+    <h2 class="section__title title--second">Porular Places</h2>
+    <p class="section__text text">
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Justo est, est
+      massa est ridiculus aliquam enim. Id eget adipiscing feugiat mauris, ac
+      non at luctus pretium. Purus, vel dui ut dolor commodo, sed. Nec blandit
+      et tincidunt euismod diam. Egestas tellus.
+    </p>
+  </section>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
+import Nav from "./components/NavComponent.vue";
 
 export default {
   name: "App",
-  components: {
-    HelloWorld,
+  components: { Nav },
+  data() {
+    return {
+      burgerVisibility: false,
+    };
+  },
+  methods: {
+    handleMenu() {
+      this.burgerVisibility = !this.burgerVisibility;
+    },
   },
 };
 </script>
 
 <style lang="scss">
+@import "./scss/Mixins.scss";
+
+.page {
+  margin: 0;
+}
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+}
+
+.header {
+  @include onMobile {
+    height: 100vh;
+    background-image: url("./assets/page_background_mobile.png");
+    background-size: cover;
+    background-repeat: no-repeat;
+    background-position-x: center;
+  }
+
+  @include onDesktop {
+    background-image: url("./assets/page_background.png");
+  }
+
+  &__top {
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    padding: 30px 20px;
+    background: linear-gradient(
+      177.82deg,
+      #eff6f8 1.83%,
+      rgba(239, 246, 248, 0) 98.17%
+    );
+  }
+
+  &__title {
+    padding: 65px 0 300px 0;
+    margin: 0;
+  }
+}
+
+.burger {
+  @include onDesktop {
+    display: none;
+  }
+
+  background-image: url("./assets/burger-ico.svg");
+  height: 12px;
+  width: 20px;
+  border: none;
+}
+
+.section {
+  background-color: #eff6f8;
+
+  &__title {
+    margin: 0;
+  }
 }
 </style>
---
+++ src/components/NavComponent.vue
@@ -0,0 +1,109 @@
+<template>
+  <nav class="nav" :class="{ active: burgerVisibility }">
+    <div class="nav__content">
+      <button class="nav__close" @click="handleMenu">X</button>
+      <ul class="nav__list list">
+        <li class="list__item item">
+          <a class="item__link link" href="#">Popular Places</a>
+        </li>
+        <li class="list__item item">
+          <a class="item__link link" href="#">Covid-19</a>
+        </li>
+        <li class="list__item item">
+          <a class="item__link link" href="#">Visa requirements</a>
+        </li>
+        <li class="list__item item">
+          <a class="item__link link" href="#">Embassy data</a>
+        </li>
+        <li class="list__item item">
+          <a class="item__link link" href="#">Customs</a>
+        </li>
+        <li class="list__item item">
+          <a class="item__link link" href="#">FAQs</a>
+        </li>
+        <li class="list__item item">
+          <button class="item__button button">Other countries</button>
+        </li>
+      </ul>
+    </div>
+  </nav>
+</template>
+
+<script>
+export default {
+  props: ["burgerVisibility"],
+  methods: {
+    handleMenu() {
+      this.$emit("close");
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+@import "../scss/Mixins.scss";
+
+.nav {
+  &__list {
+    @include onDesktop {
+      display: flex;
+      gap: 37px;
+    }
+  }
+
+  &__content {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    box-sizing: border-box;
+    height: 100vh;
+    padding: 20px 0;
+    background: #eef7ff;
+    padding: 30px 20px;
+
+    @include onDesktop {
+      background: none;
+      height: auto;
+    }
+  }
+
+  &__close {
+    @include onMobile {
+      align-self: flex-end;
+    }
+
+    @include onDesktop {
+      display: none;
+    }
+  }
+
+  @include onMobile {
+    position: fixed;
+    top: 0;
+    left: 0;
+    right: 0;
+    transform: translateX(-100%);
+    transition: transform 0.3s;
+  }
+}
+
+.list {
+  .item {
+    list-style: none;
+
+    &__link {
+      font-family: "Raleway";
+      font-style: normal;
+      font-weight: 700;
+      font-size: 18px;
+      line-height: 21px;
+
+      color: #024873;
+    }
+  }
+}
+
+.active {
+  transform: translateX(0);
+}
+</style>
